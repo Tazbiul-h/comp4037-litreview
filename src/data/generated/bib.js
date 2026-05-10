33d4da66c9c44d914751e9a9402fbb887d8a6a2f
Tazbiul Hasan <83528758+Tazbiul-h@users.noreply.github.com>
2026-05-10 18:26:20 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,102 @@
 ﻿define({ entries : {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "denkovski2022": {
+        "author": "Denkovski, S. and Khan, S.S. and Malamis, B. and Moon, S.Y. and Ye, B. and Mihailidis, A.",
+        "doi": "10.1109/ACCESS.2022.3211939",
+        "journal": "IEEE Access",
+        "keywords": "type:dataset, topic:thermal_imaging, topic:infrared, topic:fall_detection, topic:multimodal, topic:anomaly_detection",
+        "pages": "106422--106435",
+        "title": "Multi Visual Modality Fall Detection Dataset (MUVIM)",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "volume": "10",
+        "year": "2022"
+    },
+    "denkovski2024b": {
+        "author": "Denkovski, D. and Gjoreski, H. and Jovanovski, V. and Scheuter, N. and Lustrek, M.",
+        "doi": "2509.16479",
+        "journal": "arXiv Preprint",
+        "keywords": "type:method, topic:thermal_imaging, topic:fall_detection, topic:BiConvLSTM, topic:attention, topic:deep_learning, topic:real_time",
+        "title": "Thermal Imaging-Based Real-Time Fall Detection Using Motion Flow and Attention-Enhanced Convolutional Recurrent Architecture",
+        "type": "article",
+        "year": "2024"
+    },
+    "kaur2025": {
+        "author": "Kaur, A.P. and Nsugbe, E. and Drahota, A. and Oldfield, M. and Mohagheghian, I. and Sporea, R.A.",
+        "doi": "10.1016/j.bea.2025.100179",
+        "journal": "Biomedical Engineering Advances",
+        "keywords": "type:survey, topic:fall_detection, topic:wearable, topic:floor_based, topic:review",
+        "title": "State-of-the-Art Fall Detection Techniques with Emphasis on Floor-Based Systems --- A Review",
+        "type": "article",
+        "volume": "9",
+        "year": "2025"
+    },
+    "khatun2022": {
+        "author": "Khatun, M.A. and Yousuf, M.A. and Ahmed, S. and Uddin, M.Z. and Alyami, S.A. and Al-Ashhab, S. and Akhdar, H.F. and Khan, A. and Azad, A.K.M. and Moni, M.A.",
+        "doi": "10.1109/JTEHM.2022.3177710",
+        "journal": "IEEE Journal of Translational Engineering in Health and Medicine",
+        "keywords": "type:method, topic:HAR, topic:CNN, topic:LSTM, topic:attention, topic:deep_learning, topic:wearable",
+        "title": "Deep CNN-LSTM With Self-Attention Model for Human Activity Recognition Using Wearable Sensor",
+        "type": "article",
+        "volume": "10",
+        "year": "2022"
+    },
+    "mehta2020": {
+        "author": "Mehta, V. and Dhall, A. and Pal, S. and Khan, S.S.",
+        "booktitle": "Proceedings of the 25th International Conference on Pattern Recognition (ICPR)",
+        "doi": "10.1109/ICPR48806.2021.9412632",
+        "keywords": "type:method, topic:thermal_imaging, topic:fall_detection, topic:GAN, topic:anomaly_detection, topic:deep_learning",
+        "title": "Motion and Region Aware Adversarial Learning for Fall Detection with Thermal Imaging",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "ramirez2021": {
+        "author": "Ramirez, H. and Velastin, S.A. and Meza, I. and Fabregas, E. and Makris, D. and Farias, G.",
+        "doi": "10.1109/ACCESS.2021.3061626",
+        "journal": "IEEE Access",
+        "keywords": "type:method, topic:fall_detection, topic:skeleton, topic:HAR, topic:SVM, topic:RGB",
+        "pages": "33532--33542",
+        "title": "Fall Detection and Activity Recognition Using Human Skeleton Features",
+        "type": "article",
+        "volume": "9",
+        "year": "2021"
+    },
+    "riquelme2019": {
+        "author": "Riquelme, F. and Espinoza, C. and Rodenas, T. and Minonzio, J.G. and Taramasco, C.",
+        "doi": "10.3390/s19204565",
+        "journal": "Sensors",
+        "keywords": "type:dataset, topic:thermal_imaging, topic:infrared, topic:fall_detection, topic:privacy, topic:elderly",
+        "title": "eHomeSeniors Dataset: An Infrared Thermal Sensor Dataset for Automatic Fall Detection Research",
+        "type": "article",
+        "volume": "19",
+        "year": "2019"
+    },
+    "silver2025": {
+        "author": "Silver, C. and Akilan, T.",
+        "doi": "10.1016/j.engappai.2025.111819",
+        "journal": "Engineering Applications of Artificial Intelligence",
+        "keywords": "type:dataset, topic:thermal_imaging, topic:fall_detection, topic:privacy, topic:deep_learning",
+        "title": "Thermal Fall 66: A Robust Dataset for Thermal Imaging-Based Fall Detection and Eldercare",
+        "type": "article",
+        "volume": "160",
+        "year": "2025"
+    },
+    "tateno2020": {
+        "author": "Tateno, S. and Meng, F. and Qian, R. and Hachiya, Y.",
+        "doi": "10.3390/s20205957",
+        "journal": "Sensors",
+        "keywords": "type:method, topic:infrared, topic:fall_detection, topic:3D_CNN, topic:privacy, topic:low_resolution, topic:real_time",
+        "title": "Privacy-Preserved Fall Detection Method with Three-Dimensional Convolutional Neural Network Using Low-Resolution Infrared Array Sensor",
+        "type": "article",
+        "volume": "20",
+        "year": "2020"
+    },
+    "yang2024": {
+        "author": "Yang, J. and He, Y. and Zhu, J. and Lv, Z. and Jin, W.",
+        "doi": "10.3390/s24144647",
+        "journal": "Sensors",
+        "keywords": "type:method, topic:infrared, topic:fall_detection, topic:ST-GCN, topic:skeleton, topic:deep_learning, topic:privacy",
+        "title": "Fall Detection Method for Infrared Videos Based on Spatial-Temporal Graph Convolutional Network",
+        "type": "article",
+        "volume": "24",
+        "year": "2024"
     }
 }});
